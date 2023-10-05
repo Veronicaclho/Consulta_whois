d169fc05a195b90764a6fa222c576e0aa163afc4
Veronicaclho <veronicacoelho808@gmail.com>
2023-10-05 15:51:26 -0300

    Modificacao

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import whois
 
-dominio = "netflix.com" #recebe o dominio 
-resultado = whois.whois(dominio) #consultando o whois do dominio e armazena o resultado da consulta
+dominio = "netflix.com"  
+resultado = whois.whois(dominio) 
 
-if resultado.domain_name is None: #se o resultado de resultado.domain_name for igual a None, é porque o dominio nao foi encontrado
+if resultado.domain_name is None: 
 
     print('Dominio nao encontrado')
 else: 
